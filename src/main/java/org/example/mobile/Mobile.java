f4245e4c4c1750c26c7d79a600ed15c8e468744e
Prathin <pratinrp@gmail.com>
2025-12-04 23:59:19 +0530

    added Mobile.java

--- src/main/java/org/example/mobile/Mobile.java
+++ src/main/java/org/example/mobile/Mobile.java
@@ -1,19 +1,34 @@
-package org.example.Mobile;
+package org.example.mobile;
 
+import jakarta.persistence.Cacheable;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
 @Entity
+@Cacheable // enables L2 caching
 public class Mobile {
+    @Id
+    private int mobileId;
     private String mobileBrand;
     private String mobileModel;
     private int ram;
 
     public Mobile(){}
-    public Mobile(String mobileBrand, String mobileModel, int ram){
+    public Mobile(int mobileId, String mobileBrand, String mobileModel, int ram){
+        setMobileId(mobileId);
         setMobileBrand(mobileBrand);
         setMobileModel(mobileModel);
         setRam(ram);
     }
+
+    public int getMobileId(){
+        return mobileId;
+    }
+
+    public void setMobileId(int mobileId){
+        this.mobileId=mobileId;
+    }
+
     public String getMobileBrand() {
         return mobileBrand;
     }
@@ -36,5 +51,15 @@ public class Mobile {
 
     public void setRam(int ram) {
         this.ram = ram;
+    }
+
+    @Override
+    public String toString() {
+        return "Mobile{" +
+                "mobileId=" + mobileId +
+                ", mobileBrand='" + mobileBrand + '\'' +
+                ", mobileModel='" + mobileModel + '\'' +
+                ", ram=" + ram +
+                '}';
     }
 }
